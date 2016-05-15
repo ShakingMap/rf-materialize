aa4ab7b596748358604e5829c39c89c3a81df46f
zhaoyao91 <zhaoyao91@163.com>
2016-05-16 00:18:39 +0800

    update File

--- src/fields/File.js
+++ src/fields/File.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import utils from '../utils';
 
 const propTypes = {
     id: React.PropTypes.string,
@@ -6,33 +7,60 @@ const propTypes = {
     value: React.PropTypes.object, // {path, file, files}
     onChange: React.PropTypes.func,
     readOnly: React.PropTypes.bool,
-    disabled: React.PropTypes.bool
+    disabled: React.PropTypes.bool,
+
+    text: React.PropTypes.string
 
     // other props will be passed down to inner input directly
 };
 
 const defaultProps = {
-    onChange(){}
+    onChange(){},
+    text: 'file'
 };
 
 class File extends React.Component {
     render() {
-        let {id, validationState, value, onChange, readOnly, disabled, ...otherProps} = this.props;
+        let {
+            id, validationState, value, onChange, readOnly, disabled,
+            text,
+            ...otherProps
+        } = this.props;
 
         if (value === undefined) value = {path: undefined};
-        if (value === null) value = {path: ''};
+        else if (value === null) value = {path: ''};
+        const showValue = value.path === undefined ? undefined : ((value.file && value.file.name) || '');
+        const style = utils.getInputValidationStyle(validationState);
 
-        return <div className={validationState ? ('has-'+validationState):''}>
-            <input
-                id={id}
-                type="file"
-                value={value.path}
-                onChange={e=>onChange({path:e.target.value, file:e.target.files[0], files: e.target.files}, e)}
-                readOnly={readOnly}
-                disabled={disabled}
-                {...otherProps}
-            />
-        </div>
+        return <div className="file-field input-field">
+            <div className="btn">
+                <span>{text}</span>
+                <input
+                    id={id}
+                    type="file"
+                    value={value.path}
+                    onChange={e=>onChange({path:e.target.value, file:e.target.files[0], files: e.target.files}, e)}
+                    readOnly={readOnly}
+                    disabled={disabled}
+                    {...otherProps}
+                />
+            </div>
+            <div className="file-path-wrapper">
+                <input value={showValue} style={style} className="file-path" type="text" readOnly={readOnly} disabled={disabled}/>
+            </div>
+        </div>;
+
+        // return <div className={validationState ? ('has-'+validationState):''}>
+        //     <input
+        //         id={id}
+        //         type="file"
+        //         value={value.path}
+        //         onChange={e=>onChange({path:e.target.value, file:e.target.files[0], files: e.target.files}, e)}
+        //         readOnly={readOnly}
+        //         disabled={disabled}
+        //         {...otherProps}
+        //     />
+        // </div>
     }
 }
 

473fd5d4d848fad0d188f39246921f5e30119835
zhaoyao91 <zhaoyao91@163.com>
2016-05-16 00:00:52 +0800

    update Textarea

--- src/fields/Textarea.js
+++ src/fields/Textarea.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import utils from '../utils';
 
 const propTypes = {
     id: React.PropTypes.string,
@@ -20,18 +21,18 @@ class Textarea extends React.Component {
         let {id, validationState, value, onChange, readOnly, disabled, ...otherProps} = this.props;
 
         if (value === null) value = '';
+        const style = utils.getInputValidationStyle(validationState);
 
-        return <div className={validationState ? ('has-'+validationState):''}>
-            <textarea
-                id={id}
-                className="form-control"
-                value={value}
-                onChange={e=>onChange(e.target.value, e)}
-                readOnly={readOnly}
-                disabled={disabled}
-                {...otherProps}
-            />
-        </div>
+        return <textarea
+            style={style}
+            id={id}
+            className="materialize-textarea"
+            value={value}
+            onChange={e=>onChange(e.target.value, e)}
+            readOnly={readOnly}
+            disabled={disabled}
+            {...otherProps}
+        />
     }
 }
 
